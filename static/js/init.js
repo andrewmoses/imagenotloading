923ae9d00f50ae64da8704e19a27eafa178c3002
bobbyda <emmanuel@xobin.com>
2017-01-25 18:40:20 +0530

    parallax working

---
+++ static/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+function mainController($scope) {
+  $('.parallax').parallax();
+}
